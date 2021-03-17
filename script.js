c7a9c7eaa6878fc32637aae39573071f107fc39a
Your Name <joey.yashinsky@gmail.com>
2021-03-17 11:28:49 -0400

    Added 'game' object with team1 and team2 properties. will populate each team with (standard) 11 soccer players to each side.

---
+++ script.js
@@ -0,0 +1,20 @@
+const game = {
+    team1: 'USA',
+    team2: 'Brazil',
+    players: [
+        'Lalas', 
+        'Wynalda',
+        'Ramos',
+        'Preki',
+        'Howard',
+        'Lewandowski',
+        'Adu',
+        'Beasley',
+        'Donovan',
+        'Altidore',
+        'Dempsey',
+    ],
+    [
+        
+    ]
+}
