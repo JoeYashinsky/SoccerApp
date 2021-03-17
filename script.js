1e46278b94975ca908fa42cb5c33103ba6fcb20c
Your Name <joey.yashinsky@gmail.com>
2021-03-17 17:14:49 -0400

    array created called 'gameEvents' to outline all events that transpired in the game (Map utilized for no duplicate event names)

--- script.js
+++ script.js
@@ -1,83 +1,87 @@
 'use strict';
 
 const game = {
-    team1: 'USA',
-    team2: 'Brazil',
-    players: [
-      [
-        'Howard',
-        'Lalas', 
-        'Wynalda',
-        'Ramos',
-        'Preki',
-        'Lewandowski',
-        'Adu',
-        'Beasley',
-        'Donovan',
-        'Altidore',
-        'Dempsey',
-      ],
-      [
-        'Zetti',
-        'Ronaldo', 
-        'Branco', 
-        'Leonardo',
-        'Mazinho',
-        'Jorginho',
-        'Aldair',
-        'Cafu',
-        'Viola',
-        'Mauro Silva',
-        'Bebeto',
-      ],
+  team1: 'USA',
+  team2: 'Brazil',
+  players: [
+    [
+      'Howard',
+      'Lalas',
+      'Wynalda',
+      'Ramos',
+      'Preki',
+      'Lewandowski',
+      'Adu',
+      'Beasley',
+      'Donovan',
+      'Altidore',
+      'Dempsey',
     ],
-    score: '1:3',
-    scored: ['Ramos', 'Ronaldo', 'Cafu', 'Bebeto'],
-    date: 'July 4th, 1994', 
-    odds: {
-      oddsUSA: 6.5, 
-      x: 3.25,
-      oddsBrazil: 1.33,
-    },
-  };
+    [
+      'Zetti',
+      'Ronaldo',
+      'Branco',
+      'Leonardo',
+      'Mazinho',
+      'Jorginho',
+      'Aldair',
+      'Cafu',
+      'Viola',
+      'Mauro Silva',
+      'Bebeto',
+    ],
+  ],
+  score: '1:3',
+  scored: ['Ramos', 'Ronaldo', 'Cafu', 'Bebeto'],
+  date: 'July 4th, 1994',
+  odds: {
+    oddsUSA: 6.5,
+    x: 3.25,
+    oddsBrazil: 1.33,
+  },
+};
 
-  // Player array for each team (USA, Brazil)
-  const [playersUSA, playersBrazil] = game.players;
+// Player array for each team (USA, Brazil)
+const [playersUSA, playersBrazil] = game.players;
 
-  // First player will be goalie. Rest are players in field.
-  const [gkUSA, ...fieldPlayersUSA] = playersUSA;
-  console.log(gkUSA, fieldPlayersUSA);
-  // Prints:  Howard, then (10) ["Lalas", "Wynalda", etc.]
-  const [gkBrazil, ...fieldPlayersBrazil] = playersBrazil;
+// First player will be goalie. Rest are players in field.
+const [gkUSA, ...fieldPlayersUSA] = playersUSA;
+console.log(gkUSA, fieldPlayersUSA);
+// Prints:  Howard, then (10) ["Lalas", "Wynalda", etc.]
+const [gkBrazil, ...fieldPlayersBrazil] = playersBrazil;
 
-  // An array that will contain ALL players (both teams)
-  const allPlayers = [...playersUSA, ...playersBrazil];
-  console.log(allPlayers); // Prints: one single array with all 22 elements
+// An array that will contain ALL players (both teams)
+const allPlayers = [...playersUSA, ...playersBrazil];
+console.log(allPlayers); // Prints: one single array with all 22 elements
 
-  // USA inserted 3 subs. New array will consist of all original USA players plus the 3 substitutes.
+// USA inserted 3 subs. New array will consist of all original USA players plus the 3 substitutes.
 
-  const playersUSAFinal = [...playersUSA, 'Jones', 'Stewart', 'Dooley'];
+const playersUSAFinal = [...playersUSA, 'Jones', 'Stewart', 'Dooley'];
 
-  // Based on game.odds object, create a variable to represent each separate odd.
-  const {odds: {oddsUSA, x: draw, oddsBrazil}} = game;
+// Based on game.odds object, create a variable to represent each separate odd.
+const {
+  odds: { oddsUSA, x: draw, oddsBrazil },
+} = game;
 
-  // Function 'printGoals' 
-  const printGoals = function (...players) {
-      console.log(players);
-      console.log(`${players.length} goals were scored in the game.`);
-  }
+// Function 'printGoals'
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored in the game.`);
+};
 
-  printGoals('Wynalda', 'Ronaldo'); // 2 goals were scored in the game.
-  printGoals(...game.scored); // 4 goals were scored in the game.
+printGoals('Wynalda', 'Ronaldo'); // 2 goals were scored in the game.
+printGoals(...game.scored); // 4 goals were scored in the game.
 
-  // Team with lower odds more likely to win. Print to console which team more likely to win based on game.odds
-  oddsUSA < oddsBrazil && console.log(`${game.team1} is more likely to win the soccer game today.`);
-  oddsBrazil < oddsUSA && console.log(`${game.team2} is more likely to win the soccer game today.`);
+// Team with lower odds more likely to win. Print to console which team more likely to win based on game.odds
+oddsUSA < oddsBrazil &&
+  console.log(`${game.team1} is more likely to win the soccer game today.`);
+oddsBrazil < oddsUSA &&
+  console.log(`${game.team2} is more likely to win the soccer game today.`);
 
-  // Loop over game.scored array and print each player name to the console, along with the goal number. Using 'entries' will give index of each goal.
+// Loop over game.scored array and print each player name to the console, along with the goal number. Using 'entries' will give index of each goal.
 
 for (const [i, player] of game.scored.entries()) {
-    console.log(`Goal ${i + 1}: ${player}`);
+  console.log(`Goal ${i + 1}: ${player}`);
 }
 
 // Goal 1: Ramos
@@ -89,28 +93,41 @@ for (const [i, player] of game.scored.entries()) {
 const odds = Object.values(game.odds);
 let average = 0;
 for (const odd of odds) {
-    average += odd;
-    average /= odds.length;
-    console.log(average);  //  11.08 / 3 = 3.69
+  average += odd;
+  average /= odds.length;
+  console.log(average); //  11.08 / 3 = 3.69
 }
 
 // Print all 3 odds to console with key/value of each odd
-for(const [team, odd] of Object.entries(game.odds)) {
-    const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
-    console.log(team, odd);
-    // oddsUSA 6.5
-    // x 3.25
-    // oddsBrazil 1.33
-    console.log(`Odd of ${teamStr} ${odd}`);
-    //Odd of victory oddsUSA 6.5
-    //Odd of draw 3.25
-    //Odd of victory oddsBrazil 1.33
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(team, odd);
+  // oddsUSA 6.5
+  // x 3.25
+  // oddsBrazil 1.33
+  console.log(`Odd of ${teamStr} ${odd}`);
+  //Odd of victory oddsUSA 6.5
+  //Odd of draw 3.25
+  //Odd of victory oddsBrazil 1.33
 }
 
 //Create an object that contains names of all players who scored (as properties), and the number of goals scored as the associated value.
 const goalScorers = {};
 for (const player of game.scored) {
-    goalScorers[player] ? goalScorers[player]++ : (goalScorers[player] = 1);
+  goalScorers[player] ? goalScorers[player]++ : (goalScorers[player] = 1);
 }
 
-
+// Array called 'gameEvents', but without duplicates
+const gameEvents = new Map([
+  [16, 'Yellow card'],
+  [17, 'GOAL'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
